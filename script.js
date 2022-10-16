0b31dd4a78451f7d2b2c801d01bb667cb777dd8a
SidraShawaj <shawajsidra@gmail.com>
2022-10-16 21:50:04 +0200

    Score Counters
    added score counters for each the computer and user and a final announcement of the winner.

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ const computerPlay = () => {
 };
 
 const playRound = (computerSelection, playerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
   const results = `${computerSelection}, ${playerSelection}`;
-  console.log(results);
+  console.log(
+    `My choice: ${computerSelection}\nYour choice: ${playerSelection}`
+  );
 
-  switch (results) {
+  switch (results.toLowerCase()) {
     case "rock, rock":
       return `We both rock!`;
     case "rock, paper":
+      userScore++;
       return `You win! ${playerSelection} beats ${computerSelection}`;
     case "rock, scissors":
+      computerScore++;
       return `You lose! ${computerSelection} beats ${playerSelection}`;
     case "paper, rock":
+      computerScore++;
       return `You lose! ${computerSelection} beats ${playerSelection}`;
     case "paper, paper":
       return `Great.. let's make origami!`;
     case "paper, scissors":
+      userScore++;
       return `You win! ${playerSelection} beats ${computerSelection}`;
     case "scissors, rock":
+      userScore++;
       return `You win! ${playerSelection} beats ${computerSelection}`;
     case "scissors, paper":
+      computerScore++;
       return `You lose! ${computerSelection} beats ${playerSelection}`;
     case "scissors, scissors":
       return `Ouch! carefult with those!`;
@@ -34,22 +41,24 @@ const playRound = (computerSelection, playerSelection) => {
   }
 };
 
+let computerScore = 0,
+  userScore = 0;
+
 const game = () => {
   for (let i = 0; i < 5; i++) {
     const computerSelection = computerPlay();
 
-    const playerSelection = prompt(`Rock, paper or scissors?`);
+    const playerSelection = prompt(`Round ${i + 1}.\nRock, paper or scissors?`);
 
     console.log(playRound(computerSelection, playerSelection));
+    console.log(`computer score: ${computerScore}\tYour score: ${userScore}`);
   }
+
+  if (computerScore > userScore) {
+    console.log(`You lost the game :(`);
+  } else if (userScore > computerScore) {
+    console.log(`You won the game!`);
+  } else console.log(`Tie!`);
 };
 
 game();
-
-// const playerSelection = "RoCk";
-
-// const computerSelection = computerPlay();
-
-// const playerSelection = prompt(`Rock, paper or scissors?`);
-
-// console.log(playRound(computerSelection, playerSelection));
